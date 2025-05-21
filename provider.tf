c80988c5e79fbe0af6e7338aed82e25949d15bf0
dhanapal1410 <125759600+dhanapal1410@users.noreply.github.com>
2025-05-21 09:46:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIASMWR35P743GE34U4"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
