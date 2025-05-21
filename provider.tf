14d480b626a55f79c7f082d320c48636df073c91
dhanapal1410 <125759600+dhanapal1410@users.noreply.github.com>
2025-05-21 09:57:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,9 +7,11 @@ terraform {
   }
 }
 
+
 provider "aws" {
-  region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
 
