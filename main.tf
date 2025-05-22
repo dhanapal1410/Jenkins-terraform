741e0bfc7dcabaff12462fd27d4df2c3b3cd5d84
dhanapal1410 <125759600+dhanapal1410@users.noreply.github.com>
2025-05-22 11:12:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleInstance"
-  }
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
